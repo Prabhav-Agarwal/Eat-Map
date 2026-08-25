e958ba526e1503e9bbbbf1e6a61a61b685156b21
Prabhav-Agarwal <prabhavagarwal247@gmail.com>
2026-08-25 22:35:40 +0530

    Added modsl Map

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict'
 
-//Class for Application
-class app{
 
-	
-	constructor(){
+function getCoordinates(){
+	navigator.geolocation.getCurrentPosition((position)=> {
+		const {latitude , longitude} = position.coords
 
-	}
+		const modalMap = L.map('modal-map').setView([latitude, longitude], 13);
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(modalMap);
+		
+	} , (e)=> console.error(`Something went wrong : ${e.message}`))
 }
-
-
-
-
-//Class for Modal Window
-class modalWindow
+getCoordinates()
